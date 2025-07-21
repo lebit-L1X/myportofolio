fac39e5d5b7e0b5f26f59d83f45e830b44c85973
lebit-L1x <lebitl1x@gmail.com>
2025-07-22 04:55:10 +0700

    changed fonts in contact form

--- src/sections/Contact.vue
+++ src/sections/Contact.vue
@@ -1,10 +1,7 @@
 <template>
   <section class="py-20 px-6 bg-gradient-to-t from-[#140818] to-[#1c0c23] text-white relative text-center">
-  <h3 class="text-rose-400 font-medium text-sm uppercase tracking-widest mb-2">
+<h2 class="text-4xl font-bold text-rose-900 mb-2">
     Get in touch
-  </h3>
-  <h2 class="text-5xl md:text-6xl font-bold mb-6 font-[Quicksand]">
-    Contacts
   </h2>
   <p class="text-lg max-w-2xl mx-auto text-rose-200 mb-10 leading-relaxed">
     I'm always open to exciting opportunities, especially those with meaningful impact. Feel free to reach out, I’ll get back to you as soon as I can.
@@ -33,5 +30,5 @@
     </a>
   </div>
 </section>
-
+  
 </template>
